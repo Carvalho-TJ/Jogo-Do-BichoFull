4a1ce074cdb7e33672e1393ac9b1116bc7e558ef
Tiago Carvalho <tiagorc.dev@gmail.com>
2026-03-03 01:26:43 -0400

    Feat(database): Configurar o TypeORM no Nestjs para conversar com o MySQL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Permite que o Front acesse o Back
+  app.enableCors(); 
+  
+  await app.listen(3000);
+  console.log('🚀 Backend rodando em http://localhost:3000');
 }
 bootstrap();
