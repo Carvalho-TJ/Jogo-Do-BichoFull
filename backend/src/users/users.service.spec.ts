b32a536d4c7c133d88b7b45c4bf395107810921a
Tiago Carvalho <tiagorc.dev@gmail.com>
2026-03-05 14:35:30 -0400

    Test(users): corrige import do WalletsService

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { WalletsService } from '../wallets/wallets.service';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -12,6 +13,10 @@ describe('UsersService', () => {
         UsersService,
         {
           provide: getRepositoryToken(User),
+          useValue: {},
+        },
+        {
+          provide: WalletsService,
           useValue: {},
         },
       ],
