c8545ca3f0cca4bf59cb71d86d6840f037187ca0
Tiago Carvalho <tiagorc.dev@gmail.com>
2026-03-05 14:16:34 -0400

    Test: add correção no CI para teste no users, auth e wallets

--- backend/src/wallets/wallets.controller.spec.ts
+++ backend/src/wallets/wallets.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WalletsController } from './wallets.controller';
+import { WalletsService } from './wallets.service';
 
 describe('WalletsController', () => {
   let controller: WalletsController;
@@ -7,6 +8,12 @@ describe('WalletsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WalletsController],
+      providers: [
+        {
+          provide: WalletsService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<WalletsController>(WalletsController);
